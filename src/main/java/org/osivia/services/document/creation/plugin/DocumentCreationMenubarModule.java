d907e36a9cd0f43da2c39db3d7ab25ebe53732c4
Cédric Krommenhoek <cedric.krommenhoek@gmail.com>
2019-04-15 12:58:51 +0200

    :lipstick: Document creation menubar icons.

--- src/main/java/org/osivia/services/document/creation/plugin/DocumentCreationMenubarModule.java
+++ src/main/java/org/osivia/services/document/creation/plugin/DocumentCreationMenubarModule.java
@@ -1,16 +1,16 @@
 package org.osivia.services.document.creation.plugin;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
+import org.dom4j.Element;
 import org.nuxeo.ecm.automation.client.model.Document;
 import org.osivia.portal.api.Constants;
 import org.osivia.portal.api.PortalException;
 import org.osivia.portal.api.cms.DocumentContext;
-import org.osivia.portal.api.cms.EcmDocument;
-import org.osivia.portal.api.cms.PublicationInfos;
 import org.osivia.portal.api.context.PortalControllerContext;
 import org.osivia.portal.api.internationalization.Bundle;
 import org.osivia.portal.api.internationalization.IBundleFactory;
@@ -22,14 +22,12 @@ import org.osivia.portal.api.menubar.MenubarGroup;
 import org.osivia.portal.api.menubar.MenubarItem;
 import org.osivia.portal.api.menubar.MenubarModule;
 import org.osivia.portal.api.urls.PortalUrlType;
-import org.osivia.portal.core.cms.CMSException;
-import org.osivia.portal.core.cms.CMSPublicationInfos;
-import org.osivia.portal.core.cms.CMSServiceCtx;
 
 import fr.toutatice.portail.cms.nuxeo.api.NuxeoController;
 import fr.toutatice.portail.cms.nuxeo.api.cms.NuxeoDocumentContext;
 import fr.toutatice.portail.cms.nuxeo.api.cms.NuxeoPublicationInfos;
-import fr.toutatice.portail.cms.nuxeo.api.services.dao.DocumentDAO;
+import fr.toutatice.portail.cms.nuxeo.api.services.INuxeoService;
+import fr.toutatice.portail.cms.nuxeo.api.services.tag.INuxeoTagService;
 
 /**
  * @author Dorian Licois
@@ -53,6 +51,9 @@ public class DocumentCreationMenubarModule implements MenubarModule {
     /** bundleFactory */
     private final IBundleFactory bundleFactory;
 
+    /** Nuxeo service. */
+    private final INuxeoService nuxeoService;
+
 
     public DocumentCreationMenubarModule() {
         menubarService = Locator.findMBean(IMenubarService.class, IMenubarService.MBEAN_NAME);
@@ -60,6 +61,9 @@ public class DocumentCreationMenubarModule implements MenubarModule {
         IInternationalizationService internationalizationService = Locator.findMBean(IInternationalizationService.class,
                 IInternationalizationService.MBEAN_NAME);
         bundleFactory = internationalizationService.getBundleFactory(this.getClass().getClassLoader());
+
+        // Nuxeo service
+        this.nuxeoService = Locator.findMBean(INuxeoService.class, INuxeoService.MBEAN_NAME);
     }
 
     @Override
@@ -77,26 +81,55 @@ public class DocumentCreationMenubarModule implements MenubarModule {
                     boolean acceptFiles = publicationInfos.getSubtypes().contains("File");
 
                     if (acceptFiles) {
+                        // Tag service
+                        INuxeoTagService tagService = nuxeoService.getTagService();
+                        // Internationalization bundle
                         Bundle bundle = bundleFactory.getBundle(portalControllerContext.getRequest().getLocale());
+
+                        // Add dropdown
                         MenubarDropdown addDropdown = getAddDropdown(portalControllerContext, bundle);
+
                         // create WORD
+                        Element docxIcon;
+                        try {
+                            docxIcon = tagService.getMimeTypeIcon(nuxeoController, DOCX_MIMETYPE, null);
+                        } catch (IOException e) {
+                            throw new PortalException(e);
+                        }
                         MenubarItem createWordDocument = new MenubarItem("NEW_WORD_DOCUMENT", bundle.getString("NEW_WORD_DOCUMENT"),
-                                DocumentDAO.getInstance().getIcon(DOCX_MIMETYPE), addDropdown, 10, "#", null, null, null);
+                                null, addDropdown, 10, "javascript:;", null, null, null);
+                        createWordDocument.setDivider(true);
+                        createWordDocument.setCustomizedIcon(docxIcon);
                         // new word doc modal
                         createWordDocument.getData().put("target", "#osivia-modal");
                         createWordDocument.getData().put("load-url", getDocumentWordCreationPortletUrl(nuxeoController, docPath));
-                        createWordDocument.setDivider(true);
                         menubar.add(createWordDocument);
+
                         // create EXCEL
+                        Element xlsxIcon;
+                        try {
+                            xlsxIcon = tagService.getMimeTypeIcon(nuxeoController, XLSX_MIMETYPE, null);
+                        } catch (IOException e) {
+                            throw new PortalException(e);
+                        }
                         MenubarItem createExcelDocument = new MenubarItem("NEW_EXCEL_DOCUMENT", bundle.getString("NEW_EXCEL_DOCUMENT"),
-                                DocumentDAO.getInstance().getIcon(XLSX_MIMETYPE), addDropdown, 11, "#", null, null, null);
+                                null, addDropdown, 11, "#", null, null, null);
+                        createExcelDocument.setCustomizedIcon(xlsxIcon);
                         // new excel doc modal
                         createExcelDocument.getData().put("target", "#osivia-modal");
                         createExcelDocument.getData().put("load-url", getDocumentExcelCreationPortletUrl(nuxeoController, docPath));
                         menubar.add(createExcelDocument);
+
                         // create POWERPOINT
+                        Element pptxIcon;
+                        try {
+                            pptxIcon = tagService.getMimeTypeIcon(nuxeoController, PPTX_MIMETYPE, null);
+                        } catch (IOException e) {
+                            throw new PortalException(e);
+                        }
                         MenubarItem createPowerpointDocument = new MenubarItem("NEW_POWERPOINT_DOCUMENT", bundle.getString("NEW_POWERPOINT_DOCUMENT"),
-                                DocumentDAO.getInstance().getIcon(PPTX_MIMETYPE), addDropdown, 12, "#", null, null, null);
+                                null, addDropdown, 12, "#", null, null, null);
+                        createPowerpointDocument.setCustomizedIcon(pptxIcon);
                         // new ppt doc modal
                         createPowerpointDocument.getData().put("target", "#osivia-modal");
                         createPowerpointDocument.getData().put("load-url", getDocumentPowerpointCreationPortletUrl(nuxeoController, docPath));
